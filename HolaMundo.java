449a23878b785cf32e76585268c86db8beeee901
Danny Loaisiga <dannyloaisiga6@gmail.com>
2025-06-22 02:02:53 -0400

    Cambio Saludo

--- HolaMundo.java
+++ HolaMundo.java
@@ -6,6 +6,7 @@ public class HolaMundo {
  int suma = a + b;
  int multiplicacion = a *b;
  System.out.println("Hola, " + nombre + "!");
+ System.out.println("Danny Adrián Loaisiga Vivas 2025-1080U");
  System.out.println("La suma de " + a + " + " + b + " es: " + suma);
  System.out.println("La multiplicacion de " + a + " * " + b + " es: " + multiplicacion);
  }
